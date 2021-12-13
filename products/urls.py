8c6f4fbfc50a0257721c6840faf10bc5b833b179
jorge <cinfacol@gmail.com>
2021-12-12 20:03:24 -0500

    media detalle view

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, MediaDetailView
 
 app_name = "products"
 
 urlpatterns = [
-    path("<int:pk>", ProductDetailView.as_view(), name="product_detail"),
+    path("<int:pk>", MediaDetailView.as_view(), name="media_detail"),
     path("", ProductListView.as_view(), name="products"),
 ]
