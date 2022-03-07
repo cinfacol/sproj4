82e785419588e058c5b0d451fa5a54030629f4b0
jorge <cinfacol@gmail.com>
2022-03-06 22:06:38 -0500

    perfil de usuario en top_nav

--- products/views.py
+++ products/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from django.views import generic
-from perfiles.models import Profile
 
-from .models import Category, Media, Product
+from .models import Media
 
 namespace = "products"
 
@@ -13,19 +12,10 @@ class ProductListView(generic.ListView):
 
     def get(self, request):
         media = Media.objects.all()
-        # products = Product.image_product # reverse muchos a uno entre Product db y Media db
 
         context = {
             "media": media,
         }
-
-        if request.user.is_authenticated:
-            profile = Profile.objects.get(user=request.user)
-
-            context = {
-                "media": media,
-                "profile": profile,
-            }
 
         return render(request, "products/product_list.html", context)
 
@@ -34,11 +24,3 @@ class MediaDetailView(generic.DetailView):
 
     model = Media
     template_name = "products/product_detail.html"
-
-    if Profile.objects.get().user.is_authenticated:
-
-        def get_context_data(self, **kwargs):
-            context = super().get_context_data(**kwargs)
-            context["profile"] = Profile.objects.get()
-
-            return context
